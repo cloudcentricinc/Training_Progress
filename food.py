357b7c7803a3483303bebdbf89a01ed4f4330910
bhuvana masineni <bhuvanamasineni@gmail.com>
2021-03-09 12:38:13 -0600

    Salary

---
+++ food.py
@@ -0,0 +1,44 @@
+inventory = { 'gold' : 500, 'pouch' : ['flint', 'twine', 'gemstone'],
+              # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll', 'bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort()
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] = inventory['gold'] + 50
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    return total
+
+
---
+++ hobby.py
@@ -0,0 +1,11 @@
+from pip._vendor.distlib.compat import raw_input
+
+hobbies = []
+
+# Add your code below!
+
+for num in range(3):
+  hobby =  raw_input("Tell me one of your favorite hobbies: ")
+  hobbies.append(hobby)
+
+print (hobbies)
